4c6425660cb376f3dd4fd71ad98a52c4d848f9e2
Michaela Fallon <michaelafallon2@gmail.com>
2016-03-02 22:51:40 -0500

    New projects

--- services.js
+++ services.js
@@ -3,15 +3,15 @@ angular.module('myApp.services', ['ngRoute'])
         return [
             {id: "of-the-world-poster", title: "of the world, by the world"},
             {id: "warrior-girl", title: "warrior girl"},
-            {id: "consume", title: "consume"},
-            {id: "goudy", title: "goudy"},
+            {id: "technology", title: "technology map"},
+            {id: "share", title: "share catalog"},
             {id: "of-the-world-exhibit", title: "of the world, by the world"},
             {id: "selfie", title: "selfie"},
             {id: "city", title: "city"},
             {id: "traditional-book", title: "traditional book"},
-            {id: "of-the-world-interventions", title: "of the world, by the world"},
             {id: "cut-paper", title: "cut paper triptych"},
             {id: "watercolor-study", title: "watercolor study"},
-            {id: "journey", title: "journey"}
+            {id: "consume", title: "consume"},
+            {id: "goudy", title: "goudy"}
         ];
     }]);
