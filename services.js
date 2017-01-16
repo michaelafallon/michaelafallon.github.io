76c0e944f7d0d5708a647f8b3049f41a0b5f10b0
Avriel Harvey <psu.ftbl@gmail.com>
2017-01-16 18:35:00 -0500

    Updates for collaborators

--- services.js
+++ services.js
@@ -21,7 +21,8 @@ angular.module('myApp.services', ['ngRoute'])
                 "through interactive elements, allow them to explore the effects of their own " +
                 "perception on their actions. Wearable technology is used to analyze visitors " +
                 "actions, which produces data that visitors are able to reflect upon.",
-                date: "Fall 2016"
+                date: "Fall 2016",
+                collaborators: "In collaboration with Ali Hartzoge, Tara Sullivan, and Matthew Wright"
             },
             {
                 id: "revolutions",
@@ -29,26 +30,30 @@ angular.module('myApp.services', ['ngRoute'])
                 description: "This publication is designed within a typographic system. The topic is on " +
                 "motorcycle clubs and contains feature and supporting articles, which are " +
                 "differentiated by the typographic rules and roles.",
-                date: "Fall 2016"
+                date: "Fall 2016",
+                collaborators: ""
             },
             {
                 id: "selfie",
                 title: "Selfie",
                 description: "Blindline pen and ink drawing, painted with watercolors.",
-                date: "Spring 2014"
+                date: "Spring 2014",
+                collaborators: ""
             },
             {
                 id: "metropolis",
                 title: "Metropolis Magazine Analysis",
                 description: "A publication analyzing the design structure and aesthetic of <em>Metropolis</em> magazine. " +
                 "The analysis publication is designed in light of <em>Metropolis</em>.",
-                date: "Fall 2016"
+                date: "Fall 2016",
+                collaborators: "In collaboration with Courtney Banning and Emma Wegmiller"
             },
             {
                 id: "moving-type",
                 title: "Moving Typography",
                 description: "An exploration in dynamic typography. The videos shows the navigation from a table of contents to the body text or an article.",
-                date: "Fall 2016"
+                date: "Fall 2016",
+                collaborators: ""
             },
             {
                 id: "wedding",
@@ -56,7 +61,8 @@ angular.module('myApp.services', ['ngRoute'])
                 description: "This invitation and the additional materials were designed for a winter wedding. " +
                 "The bride and groom wanted a modern twist on the traditional wedding aesthetic " +
                 "and structure.",
-                date: "Fall 2016"
+                date: "Fall 2016",
+                collaborators: ""
             },
             {
                 id: "retrace",
@@ -64,7 +70,8 @@ angular.module('myApp.services', ['ngRoute'])
                 description: "The challenge  was to create something that enhanced the experience of a house museum in " +
                 "downtown Raleigh, so that it would be a more attractive sight. An app, foldable map, " +
                 "and interactive memory archive were created to tie this sight into more historic places in downtown Raleigh.",
-                date: "Fall 2016"
+                date: "Fall 2016",
+                collaborators: "In collaboration with Shadrick Addy, Abby Gartner, Nate Winskunas, Eduardo Talavera, and Lucia Boehling"
             },
             {
                 id: "tech",
@@ -72,13 +79,15 @@ angular.module('myApp.services', ['ngRoute'])
                 description: "Information about the use of technology in Hunt Library was collected through " +
                 "research, interviews, and observations. The conclusions were organized and " +
                 "displayed on this knowledge map.",
-                date: "Spring 2016"
+                date: "Spring 2016",
+                collaborators: ""
             },
             {
                 id: "warrior",
                 title: "Warrior Girl & City Print",
                 description: "Three-color linoleum block print.",
-                date: "Spring 2013"
+                date: "Spring 2013",
+                collaborators: ""
             }
         ];
     }]);
