f12a3db88f8c910f276fba3c4b7975bd04ab31e8
Avriel Harvey <psu.ftbl@gmail.com>
2017-02-17 23:11:59 -0500

    Added newapp

---
+++ newapp/viewer/viewer.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('myApp.viewer', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/viewer', {
+            templateUrl: 'viewer/viewer.html',
+            controller: 'ViewerCtrl'
+        });
+    }])
+
+    .controller('ViewerCtrl', ['$scope', '$routeParams', '$http', '$sce', 'Data', function ($scope, $routeParams, $http, $sce, Data) {
+
+        angular.forEach(Data, function(image) {
+            console.log($routeParams.id);
+            console.log(image.id);
+
+            if ($routeParams.id == image.id) {
+                $scope.jsonData = image;
+            }
+        });
+
+        $scope.renderHtml = function(html_code)
+        {
+            return $sce.trustAsHtml(html_code);
+        };
+
+        $scope.fragmentFile = "fragments/" + $routeParams.id + ".html";
+
+    }]);
